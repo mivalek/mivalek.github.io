fd996aacd421dc3505ca2eef1f65fc5438a5704c
mivalek <mivalek@gmail.com>
2020-07-21 19:02:56 +0100

    copyCode function added

--- sheet_files/sheets.js
+++ sheet_files/sheets.js
@@ -39,4 +39,17 @@ $(window).resize(my_offset);
 $(window).resize(toc_width);
 $(window).resize(f);
 
+function copyCode(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = $(text).text();
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
 </script>
