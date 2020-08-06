3b8d56ed48fac4bd2224fb73367fe3724688c0c9
mivalek <mivalek@gmail.com>
2020-08-06 17:50:40 +0100

    nearly there?

--- viz/regression_line/reg_line.js
+++ viz/regression_line/reg_line.js
@@ -7,7 +7,6 @@
 
 const sliderToValue = (value, slope) => {
   let out = Math.abs(Math.round(value * 100) / 100);
-  if (value != 0) {
     if (slope) {
       if (value == 1) {
         out = " + x";
@@ -16,9 +15,6 @@ const sliderToValue = (value, slope) => {
       }
     }
     if (value < 0) out = '&minus; ' + out;
-  } else {
-    out = "";
-  }
   return out;
 }
 
@@ -31,23 +27,18 @@ const updateInterceptValue = (value) => {
   interceptOut.innerHTML = sliderToValue(value, false);
 }
 const updateSlopeValue = (value) => {
-  if (value == 0) {
-    document.getElementById("x").style.setProperty("display", "none");
+  if (value < 0) {
+    slopeOut.classList.add('negative');
   } else {
-    if (value < 0) {
-      slopeOut.classList.add('negative');
-    } else {
-      slopeOut.classList.remove('negative');
-    }
-    document.getElementById("x").style.setProperty("display", "inline");
+    slopeOut.classList.remove('negative');
   }
   slopeOut.innerHTML = sliderToValue(value, true);
 }
 
 
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-      width = 460 - margin.left - margin.right,
+const margin = {top: 10, right: 30, bottom: 70, left: 60},
+      width = 500 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 let interceptIn = document.getElementById('intercept'),
     interceptOut = document.getElementById('intercept-value'),
@@ -85,8 +76,8 @@ const g = d3.select("#plot")
           "translate(" + margin.left + "," + margin.top + ")");
 
 
-let intercept,
-    slope;
+let intercept = 0,
+    slope = 0;
 const xScale = d3.scaleLinear()
             .domain([-1, 5])
             .range([0, width]),
@@ -102,6 +93,19 @@ g.append("g")
   .attr("transform", "translate(0," + (height + 10) + ")")
   .call(d3.axisBottom(xScale)
           .ticks(7));
+
+g.append("text")
+  .attr("text-anchor", "middle")
+  .attr("x", width/2)
+  .attr("y", height + margin.bottom)
+  .text("x");
+
+  g.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", 10 - margin.left)
+    .attr("y", (height + margin.top)/2)
+    .text("y");
+
 g.append("g")
   .call(d3.axisLeft(yScale))
   .attr("transform", "translate(-10, 0)");
@@ -122,8 +126,8 @@ g.append("line")
   .attr("x2", xScale(1))
   .attr("y2", yScale(10))
   .attr("stroke-width", 0.5)
-  .attr("stroke", "#ccc")
-  .style("stroke-dasharray", ("3, 3"));
+  .attr("stroke", "#888")
+  .style("stroke-dasharray", ("4, 5"));
 
 g.append("clipPath")
   .attr("id", "clip")
@@ -138,12 +142,6 @@ function updatePlot() {
   d3.select("#gg").remove()
   intercept = +interceptIn.value;
   slope = +slopeIn.value;
-  const zero = document.getElementById("zero")
-  if (!(intercept || slope)) {
-    zero.style.setProperty("display", "inline");
-  } else {
-    zero.style.setProperty("display", "none");
-  }
 
   const gg = g.append("g")
     .attr("id", "gg")
@@ -153,13 +151,13 @@ function updatePlot() {
     .attr("cx", xScale(0))
     .attr("cy", yScale(intercept))
     .attr("r", rScale(1))
-    .attr("fill", "#33a0e15e");
+    .attr("fill", "rgba(51, 160, 225, .35)");
 
   gg.append("circle")
     .attr("cx", xScale(1))
     .attr("cy", yScale((intercept + (intercept + slope))/2))
     .attr("r", rScale(Math.abs(slope/4)))
-    .attr("fill", "#ffe44e78")
+    .attr("fill", "rgba(255, 228, 78, .4)")
     .attr("transform", "translate(10, 0)");
 
   // slope indicator
@@ -197,8 +195,8 @@ function updatePlot() {
     .attr("x2", xScale(5))
     .attr("y2", yScale(intercept))
     .attr("stroke-width", 0.5)
-    .attr("stroke", "#ccc")
-    .style("stroke-dasharray", ("3, 3"));
+    .attr("stroke", "#888")
+    .style("stroke-dasharray", ("4, 5"));
 
   // regression line
   gg.append("line")
@@ -210,6 +208,7 @@ function updatePlot() {
      .attr("stroke", "orangered");
 }
 
-updatePlot();
+
 updateInterceptValue(intercept);
 updateSlopeValue(slope);
+updatePlot();
